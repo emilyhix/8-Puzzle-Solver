4f436fd327f8b18e080bf8f618debe7e628d2799
bphimmala <bphimmala@gmail.com>
2024-10-31 03:06:23 -0700

    started code for search function(s)

--- src/Search.cpp
+++ src/Search.cpp
@@ -6,6 +6,28 @@ void Search(int algorithm, Node initialNode) {
     priority_queue<Node> frontier;
     priority_queue<Node> explored;
 
+    int i = 1;
+
+    while(i == 1){
+        if (frontier.empty()){
+            return;
+        }
+    }
+
+    
+
     return;  
+}
+
+// temporary function so that Baipon doesn't get confused
+void AStarMisplacedTilesSearch(Node initialNode){
+    priority_queue<Node> frontier;
+    priority_queue<Node> explored;
+
+    if(frontier.empty()){
+        return;
+    }
+
+    return;
 }
 
